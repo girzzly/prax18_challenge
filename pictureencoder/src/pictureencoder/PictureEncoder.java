8752ff8dc58f9abaad1aa3318b801568e799e6bf
Dominik Krakau <plutomars_de@yahoo.de>
2018-09-13 20:21:42 +0200

    pictureencoder bearbeitet,

--- pictureencoder/src/pictureencoder/PictureEncoder.java
+++ pictureencoder/src/pictureencoder/PictureEncoder.java
@@ -5,17 +5,16 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 /**
  *
  * @author Michael Sorban
  */
 public class PictureEncoder {
-    
+
     private static String directory;
     private static String file;
+    private static String transforationType;
 
     /**
      * @param args the command line arguments
@@ -23,88 +22,76 @@ public class PictureEncoder {
     public static void main(String[] args) {
         
         directory = "";
-//        file = args[0];
-        file = "tesla.png";
-        
-        encode(directory, file);
-//        decode(directory, file);
-        
-          
-        
-    }
-    
-    public static void encode(String directory, String file) {
-        
-        File inputFile = new File(directory + file);
-        File outputFile = new File(directory + "encoded_" + file);
-        
-        InputStreamReader reader;
-        OutputStreamWriter writer;
-        
-        try {
-            
-            reader = new InputStreamReader(new FileInputStream(inputFile));
-            writer = new OutputStreamWriter(new FileOutputStream(outputFile));
-            
-            int sign = reader.read();
-         
-            while(sign != -1) {
-                
-                System.out.println(sign);
-                writer.write(sign);
-                sign = reader.read();
-            }
-            
-            reader.close();
-            writer.close();
-            
-            System.out.println("Datei wurde erstellt.");
-            
-            
-        } catch(FileNotFoundException e) {
-            
-            System.out.println("Datei nicht gefunden.");
-            
-        } catch(IOException e) {
-            
-            System.out.println("Datei ist beschädigt.");
+
+        switch (args.length) {
+            case 2:
+                file = args[0];
+                transforationType = args[1];
+                transform(directory, file, transforationType);
+                break;
+            case 1:
+                System.out.println("Bitte geben Sie einen Parameter an.\nVerwenden Sie 'encode' oder 'decode'.");
+                break;
+            case 0:
+                System.out.println("Bitte geben Sie einen Dateinamen an.");
+                break;
+            default:
+                System.out.println("Sie haben zu viele parameter verwendet.\nVerwenden Sie nur 'encode' oder 'decode'.");
+                break;
         }
     }
-    
-    public static void decode(String directory, String file) {
-        
-        FileInputStream reader;
-        FileOutputStream writer;
-        
-        int sign;
-        
-        try {
-            
-            reader = new FileInputStream(directory + file);
-            writer = new FileOutputStream(directory + "decoded_" + file);
-            
-            sign = reader.read();
-            
-            while(sign > 0) {
+
+    public static void transform(String directory, String file, String transformationType) {
+
+        if (transformationType.equals("encode") || transformationType.equals("decode")) {
+
+            File inputFile = new File(directory + file);
+            File outputFile = new File(directory + transformationType + "_" + file);
+
+            FileInputStream reader;
+            FileOutputStream writer;
+
+            try {
+
+                reader = new FileInputStream(inputFile);
+                writer = new FileOutputStream(outputFile);
+
+                int sign = reader.read();
+
+                while (sign != -1) {
+
+                    if (transforationType.equals("encode")) {
+
+                        writer.write(sign + 1);
+
+                    } else if (transforationType.equals("decode")) {
+
+                        writer.write(sign - 1);
+                    }
+
+                    sign = reader.read();
+                }
+
+                reader.close();
+                writer.close();
                 
-                System.out.println(sign);
-                writer.write(sign - 7);
-                sign = reader.read();
+                if(transforationType.equals("encode")) {
+                    System.out.println("Datei wurde erfolgreich codiert.");
+                } else if(transforationType.equals("decode")) {
+                    System.out.println("Datei wurde erfolgreich decodiert.");
+                }
+
+            } catch (FileNotFoundException e) {
+
+                System.out.println("Datei nicht gefunden.");
+
+            } catch (IOException e) {
+
+                System.out.println("Datei ist beschädigt.");
             }
+        } else {
             
-            reader.close();
-            writer.close();
-            
-            System.out.println("Datei wurde erstellt.");
-            
-            
-        } catch(FileNotFoundException e) {
-            
-            System.out.println("Datei nicht gefunden.");
-            
-        } catch(IOException e) {
-            
-            System.out.println("Datei ist beschädigt.");
+            System.out.println("Falsche Parameter verwendet.\nNutzen sie bitte die Parameter 'encode' oder 'decode'.");
         }
     }
 }
